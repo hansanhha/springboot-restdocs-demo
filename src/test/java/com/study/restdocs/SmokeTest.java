d5c247c61509a34adbd5dc8014f6bf8f7484f6ff
hansanhha <kwixs35@gmail.com>
2023-12-25 13:13:31 +0900

    @SpringBootTest application load test 추가

---
+++ src/test/java/com/study/restdocs/SmokeTest.java
@@ -0,0 +1,19 @@
+package com.study.restdocs;
+
+import com.study.restdocs.controller.HomeController;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SmokeTest {
+
+    @Autowired
+    private HomeController controller;
+
+    @Test
+    void contextLoads() {
+        Assertions.assertThat(controller).isNotNull();
+    }
+}
