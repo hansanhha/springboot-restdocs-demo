b0a93f178c3c2f76d480d8d26e074ec510ea4aed
hansanhha <kwixs35@gmail.com>
2023-12-25 14:47:40 +0900

    MockBean 테스트 (basic)

---
+++ src/test/java/com/study/restdocs/WebMockTest.java
@@ -0,0 +1,34 @@
+package com.study.restdocs;
+
+import com.study.restdocs.controller.HomeController;
+import com.study.restdocs.service.GreetingService;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(HomeController.class)
+public class WebMockTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private GreetingService greetingService;
+
+    @Test
+    void greetingShouldReturnMessageFromService() throws Exception {
+        Mockito.when(greetingService.greet()).thenReturn("Hello, World");
+        mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(containsString("Hello, World")));
+    }
+}
